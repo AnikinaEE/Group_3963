1c201f74cc38c90da5274b5fac1f675a58e5551c
Elena Anikina <faniya@bk.ru>
2023-02-14 09:30:31 +0300

    add Task5 Seminar1

---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №5
+// Напишите программу, которая на вход принимает
+// одно число (N), а на выходе показывает все целые
+// числа в промежутке от -N до N
+
+Console.WriteLine("Введите число: ");
+string? lineN = Console.ReadLine();
+
+if (lineN != null)
+{
+    int N = int.Parse(lineN);
+    string res = string.Empty;
+    int negN = N * (-1);
+    while (negN <= N)
+    {
+        res = res + negN + " ";
+        negN = negN + 1;
+    }
+    Console.WriteLine(res);
+}
