23514895a2467a3d4cd6f9593f8ab3f9adbd1bb2
Elena Anikina <faniya@bk.ru>
2023-02-14 09:31:28 +0300

    add Task7 Seminar1

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,14 @@
+﻿// задача №7
+// Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе
+// показывает последнюю цифру этого числа
+
+Console.WriteLine("Введите число: ");
+string? lineN = Console.ReadLine();
+
+if (lineN != null)
+{
+    int N = int.Parse(lineN);
+    int res = N % 10;
+    Console.WriteLine("Последняя цифра числа: " + res);
+}
