8679bc628e49321137a5485c1bd433c049a732e0
Elena Anikina <faniya@bk.ru>
2023-02-14 09:29:55 +0300

    add Task3 Seminar1

---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача №3
+// Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру
+
+Console.WriteLine("Введите номер дня недели: ");
+string? lineDay = Console.ReadLine();
+
+if (lineDay != null)
+{
+    int day = int.Parse(lineDay);
+
+    string[] dayWeek = new string[7];
+    dayWeek[0] = "понедельник";
+    dayWeek[1] = "вторник";
+    dayWeek[2] = "среда";
+    dayWeek[3] = "четверг";
+    dayWeek[4] = "пятница";
+    dayWeek[5] = "суббота";
+    dayWeek[6] = "воскресение";
+
+    Console.WriteLine(dayWeek[day-1]);
+
+    // string outDayOfWeek = System.Globalization.CultureInfo.GetCultureInfo("ru-Ru").DateTimeFormat.GetDayName((DayOfWeek)Enum.GetValues(typeof(DayOfWeek)).GetValue(day));
+    // Console.WriteLine(outDayOfWeek);
+}
