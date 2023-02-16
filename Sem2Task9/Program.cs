9c234181f5c054e22402d0421a73ce8040d36c05
Elena Anikina <faniya@bk.ru>
2023-02-16 12:04:45 +0300

    add Sem2Task9

---
+++ Sem2Task9/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача №9
+// Напишите программу, которая выводит случайное
+// число из отрезка [10, 99] и показывает наибольшую
+// цифру числа
+
+System.Random numSintesator = new System.Random();
+
+// Определяем случайное число от 10 до 99 и выводим его на экран
+int rndNumber = numSintesator.Next(10, 100);
+Console.WriteLine(rndNumber); 
+
+// Определяем первую и вторую цифру нашего случайного числа
+int firstNum = rndNumber / 10;
+int secondNum = rndNumber % 10;
+
+if(firstNum > secondNum)
+{
+    Console.WriteLine("Наибольшая цифра числа: " + firstNum);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра числа: " + secondNum);
+}
+
+// // Представим число в виде массива (char - тип символы) в строковом представлении (ToString) 
+// char[] digits = numSintesator.Next(10, 100).ToString().ToCharArray();
+// Console.WriteLine(digits);
+
+// int firstNum = ((int)digits[0])-48;
+// int secondNum = ((int)digits[1])-48;
+
+// int resultNumber = firstNum > secondNum ? resultNumber = firstNum : resultNumber = secondNum;
+
+// Console.WriteLine($"Наибольшая цифра числа: {resultNumber}");
+
