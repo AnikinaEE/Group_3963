59d47da833d8d97f696f9575e9736d8f150a5fc4
Elena Anikina <faniya@bk.ru>
2023-02-21 11:49:54 +0300

    add Task17 Seminar3

---
+++ Sem3Task17/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача №17
+// Напишите программу, которая принимает на вход
+// координаты точки (x,y), причем x не равно 0 и
+// y не равно 0, и выдает черверть плоскости, 
+// в которой находится эта точка
+
+
+// <тип> <имя метода>(<тип> <название переменной>)
+// {
+// return <тип>
+// }
+
+// Метод считывания данных, вводимых пользователем
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()?? "0");
+}
+
+// Метод вывода данных
+void PrintQuterTest(int x, int y)
+{
+    if (x > 0 && y > 0) Console.WriteLine("Точка находится в I четверти плоскости");
+    if (x > 0 && y < 0) Console.WriteLine("Точка находится во II четверти плоскости");
+    if (x < 0 && y < 0) Console.WriteLine("Точка находится в III четверти плоскости");
+    if (x < 0 && y > 0) Console.WriteLine("Точка находится в IV четверти плоскости");
+}
+
+int coordX = ReadData("Введите координату X: ");
+int coordY = ReadData("Введите координату Y: ");
+PrintQuterTest(coordX, coordY);
