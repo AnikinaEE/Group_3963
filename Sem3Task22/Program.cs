9f42e866aa652af600489220d2b9afee5383182d
Elena Anikina <faniya@bk.ru>
2023-02-21 11:53:08 +0300

    add Task22 Seminar3

---
+++ Sem3Task22/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача №22
+// Напишите программу, которая на вход принимает
+// число N и выдает таблицу квадратов чисел
+// от 1 до N
+
+// Метод считывания данных пользователя
+int ReadData(string mess)
+{
+    Console.WriteLine(mess);
+    return int.Parse(Console.ReadLine()?? "0");
+}
+
+// Метод вычисления квадратов чисел
+string LineBuilder(int num, int pow)
+{
+    string rez = string.Empty;
+    for (int i = 1; i <= num; i++)
+    {
+        rez = rez + Math.Pow(i,pow) + "\t";       // Math.Pow(i,pow)+"\t"
+    }
+    return rez;
+}
+
+int N = ReadData("Введите число: ");
+
+Console.WriteLine(LineBuilder(N,1));
+Console.WriteLine(LineBuilder(N,2));
