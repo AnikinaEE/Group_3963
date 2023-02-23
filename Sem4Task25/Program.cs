16a525dab3954b8e1480f513a4282e2c15cbf22f
Elena Anikina <faniya@bk.ru>
2023-02-23 22:12:42 +0300

    add Task25 Seminar4

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача №25
+// Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B
+
+// Метод ввода информации от пользователя
+int ReadData(string mess)
+{
+    Console.WriteLine(mess);
+    return int.Parse(Console.ReadLine()?? "0");     // При вводе сразу осуществляем проверку на пустое значение - NULL
+}
+
+int numberA = ReadData("Введите число A: ");
+int numberB = ReadData("Введите число B: ");
+
+// Метод возведения числа А в степень В
+long Pow(int numA, int numB)
+{
+    long res = 1;
+    while (numB>0)
+    {
+        res = res*numA;
+        numB = numB-1;
+    }
+    return res;
+} 
+
+// Метод вывода результата
+void PrintResalt(string output)
+{
+    Console.WriteLine(output);
+} 
+PrintResalt("Число А, возведенное в степень В, равно: " + Pow(numberA, numberB));
+
