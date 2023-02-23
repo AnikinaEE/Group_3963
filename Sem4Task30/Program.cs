a0d5c261051d4e0ba99ae6d2e99aaa5c20edafe8
Elena Anikina <faniya@bk.ru>
2023-02-23 22:15:19 +0300

    add Task30 Seminar4

---
+++ Sem4Task30/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача №30 
+// Напишите программу, которая выводит массив из 8
+// элементов, заполненный нулями и единицами в
+// случайном порядке
+
+// Метод вывода массива
+void Print1DArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i=0; i<arr.Length-1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length-1]);
+    Console.WriteLine("]");
+}
+
+// Метод заполнения массива в случайном порядке диапазопа (min,max)
+int[] Gen1DArr()   
+{
+    int[] arr = new int[8];    // new - выделение памяти
+    for (int i=0; i<8; i++)
+    {
+        arr[i] = new Random().Next(0,2);
+    }
+    return arr;
+}
+
+Print1DArr(Gen1DArr());
