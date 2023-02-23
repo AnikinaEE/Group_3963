561380110b1521b3b53d2e3d1899c978d2e543e8
Elena Anikina <faniya@bk.ru>
2023-02-23 22:13:18 +0300

    add Task26 Seminar4

---
+++ Sem4Task26/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача №26
+// Напишите программу, которая принимает на вход
+// число и выдаёт количество цифр в числе
+
+// Метод ввода информации от пользователя
+int ReadData(string mess)
+{
+    Console.WriteLine(mess);
+    return int.Parse(Console.ReadLine()?? "0");     // При вводе сразу осуществляем проверку на пустое значение - NULL
+}
+
+int number = ReadData("Введите число: ");
+
+// Метод подсчета количества цифр в веденном числе
+int DigitSum(int num)
+{
+    int res = 0;    // res = num.ToString.Length;  res = Math.Log(num)+1;
+    while (num > 0)
+    {
+        res+=1;
+        num = num/10;
+    }
+    return res;
+}
+
+int sum = DigitSum(number);
+
+// Метод вывода результата
+void PrintResalt(string output)
+{
+    Console.WriteLine(output);
+} 
+PrintResalt("Количесиво цифр во введенном числе: " + sum);
+
