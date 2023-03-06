59ed2e7c031649eb232ef32f46fb7f0352be17f8
Elena Anikina <faniya@bk.ru>
2023-03-06 15:46:11 +0300

    add Task44 Seminar6

---
+++ Sem6Task44/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача №44
+// Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1
+
+int ReadData(string mess)
+{
+    Console.WriteLine(mess);
+    return int.Parse(Console.ReadLine()?? "0");     // При вводе сразу осуществляем проверку на пустое значение - NULL
+}
+
+int numN = ReadData("Введите число N: ");
+
+string FibNum(int num)
+{
+    string res = "0 1";
+    int first = 0;
+    int last = 1;
+    int temp = 0;
+
+    for (int i=1; i<num; i++)
+    {
+        res = res + " " + (first+last).ToString();
+        temp = last;
+        last = last+first;
+        first = temp;
+    }
+    return res;
+}
+
+// Метод вывода полученного результата
+void PrintData(string mess)
+{
+    Console.WriteLine(mess);
+}
+
+PrintData(FibNum(numN));
