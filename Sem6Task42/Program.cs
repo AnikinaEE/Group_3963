bcaae4bce545b8939892d49271599e64fdef11f2
Elena Anikina <faniya@bk.ru>
2023-03-06 15:45:16 +0300

    add Task42 Seminar6

---
+++ Sem6Task42/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача №42
+// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное
+
+// Метод ввода информации от пользователя
+int ReadData(string mess)
+{
+    Console.WriteLine(mess);
+    return int.Parse(Console.ReadLine()?? "0");     // При вводе сразу осуществляем проверку на пустое значение - NULL
+}
+
+int numN = ReadData("Введите число: ");
+
+// Метод перевода числа в двоичную систему счисления
+string DecToBin(int num)
+{
+    string res = string.Empty;
+    while (num > 0)
+    {
+        res = num%2 + res;
+        num = num/2;
+    }
+    return res;        
+}
+
+// // Альтернативный метод перевода числа в двоичную систему счисления (метод С#)
+// string DecToBin2(int num, int baseI)
+// {
+//     return Convert.ToString(num, baseI);     // baseI - система счисления, в которую необходимо перевести число (2, 8, 16)
+// }
+
+// Метод вывода полученного результата
+void PrintData(string mess)
+{
+    Console.WriteLine(mess);
+}
+
+// Вывод полученного результата
+string result = DecToBin(numN);
+PrintData($"Десятичное число {numN} в двоичном счислении равно: " + result);
+
