a05911cc2e6d15e81beadd7f5a91ffb7fc718498
Elena Anikina <faniya@bk.ru>
2023-03-10 12:44:22 +0300

    add Task53 Seminar8

---
+++ Sem8Task53/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача №53
+// Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю
+// строку массива
+
+// Метод ввода информации от пользователя
+int ReadData(string mess)
+{
+    Console.WriteLine(mess);
+    return int.Parse(Console.ReadLine() ?? "0");     // При вводе сразу осуществляем проверку на пустое значение - NULL
+}
+
+// Метод заполнения двумерного массива
+int[,] Gen2DArr(int countRow, int countColumn, int min, int max)
+{
+    if (min > max) (min, max) = (max, min);
+
+    int[,] arr = new int[countRow, countColumn];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+// Метод вывода на печать двумерного массива
+void Print2DArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Метод замены местами первой и последней строки массива
+void Change2DArr(int[,] matr)
+{
+    int temp = 0;
+    for (int i=0; i<matr.GetLength(1); i++)
+    {
+        temp = matr[0,i];
+        matr[0,i] = matr[matr.GetLength(0)-1,i];
+        matr[matr.GetLength(0)-1,i] = temp;
+    }
+}
+
+// Принимаем от пользователя значения размера матрицы
+int row = ReadData("Введите количество строк массива: ");
+int column = ReadData("Введите количество столбцов массива: ");
+Console.WriteLine();
+
+// Заполняем матрицу случайными целыми числами и выводим на печать
+int[,] array2D = Gen2DArr(row, column, 10, 99);
+Print2DArr(array2D);
+Console.WriteLine();
+
+// Производим замену первой и последней строки матрицы и выводим результат
+Change2DArr(array2D);
+Print2DArr(array2D);
