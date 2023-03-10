affa27d003737fd6861b644e2cd00568e133c9d2
Elena Anikina <faniya@bk.ru>
2023-03-10 12:44:56 +0300

    add Task54 Seminar8

---
+++ Sem8Task54/Program.cs
@@ -0,0 +1,89 @@
+﻿// Задача №54 
+// Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива
+
+// Метод ввода информации от пользователя
+int ReadData(string mess)
+{
+    Console.WriteLine(mess);
+    return int.Parse(Console.ReadLine() ?? "0");     // При вводе сразу осуществляем проверку на пустое значение - NULL
+}
+
+// Метод заполнения двумерного массива
+int[,] Gen2DArr(int countRow, int countColumn, int min, int max)
+{
+    if (min > max) (min, max) = (max, min);
+
+    int[,] arr = new int[countRow, countColumn];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+// Метод вывода на печать двумерного массива
+void Print2DArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+// Метод сортировки строки массива пузырьком
+void BubbleSort(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if (arr[i] < arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+void Sort2DArr(int[,] arr)
+{
+    int[] row = new int[arr.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            row[j] = arr[i, j];
+        }    
+        BubbleSort(row);
+        for (int z = 0; z < row.Length; z++)
+        {
+            arr[i, z] = row[z];
+        }
+    }
+}
+
+Console.Clear();
+
+// Принимаем от пользователя значения размера матрицы
+int row = ReadData("Введите количество строк массива: ");
+int column = ReadData("Введите количество столбцов массива: ");
+Console.WriteLine();
+
+// Заполняем матрицу случайными целыми числами и выводим на печать
+int[,] array2D = Gen2DArr(row, column, 10, 99);
+Print2DArr(array2D);
+Console.WriteLine();
+
+// Сортируем строки по убыванию и выводим упорядоченный массив
+Sort2DArr(array2D);
+Print2DArr(array2D);
